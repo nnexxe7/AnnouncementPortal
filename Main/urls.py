76666f98b3a237c630022c40344950febd494b9e
nnexxe <michal21x@gmail.com>
2024-06-04 01:11:16 +0200

    Add Products app and home.html

--- Main/urls.py
+++ Main/urls.py
@@ -17,7 +17,9 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import path
+from Products import views
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path('', views.home, name='home'),
 ]
