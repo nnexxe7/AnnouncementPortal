9dfa30d00dbe494f6c3f16e20219a21e3d3e8951
nnexxe <michal21x@gmail.com>
2024-06-07 00:44:26 +0200

    Add basic css file to our home.html

--- Main/settings.py
+++ Main/settings.py
@@ -117,9 +117,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = "static/"
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-
+STATIC_URL = "Static/"
+MEDIA_ROOT = os.path.join(BASE_DIR, 'Media')
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'Static'),
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
