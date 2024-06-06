1db33060c00776242f6fb583cbef0f6708d5b397
nnexxe <michal21x@gmail.com>
2024-06-07 00:34:04 +0200

    Add a receiver to delete images automatically

--- Products/models.py
+++ Products/models.py
@@ -1,5 +1,9 @@
 from django.db import models
 from django.contrib.auth.models import User
+import os
+from django.db.models.signals import post_delete
+from django.dispatch import receiver
+
 
 class Category(models.Model):
     name = models.CharField(max_length=100)
@@ -12,9 +16,21 @@ class Listing(models.Model):
     description = models.TextField()
     price = models.DecimalField(max_digits=10, decimal_places=2)
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
-    image = models.ImageField(upload_to='listing_images/', blank=True)
+    image = models.ImageField(upload_to='listing_images/')
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.title
+
+    def delete(self, *args, **kwargs):
+        if self.image:
+            if os.path.isfile(self.image.path):
+                os.remove(self.image.path)
+        super().delete(*args, **kwargs)
+
+@receiver(post_delete, sender=Listing)
+def delete_listing_image(instance, **kwargs):
+    if instance.image:
+        if os.path.isfile(instance.image.path):
+            os.remove(instance.image.path)
