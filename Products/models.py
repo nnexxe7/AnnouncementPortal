f1c93c3e711c7eac8df8185b6c1d1aa857dae987
nnexxe <michal21x@gmail.com>
2024-06-09 21:49:33 +0200

    Add few categories and add sorting by name

--- Products/models.py
+++ Products/models.py
@@ -8,6 +8,9 @@ from django.dispatch import receiver
 class Category(models.Model):
     name = models.CharField(max_length=100)
 
+    class Meta:
+        ordering = ['name']
+
     def __str__(self):
         return self.name
 
